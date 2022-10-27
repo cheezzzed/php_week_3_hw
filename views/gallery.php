b3bb240ce98baee44fe2861288e7c0cb09c49f88
Johnny Sanchez <johnny062999@gmail.com>
2022-10-27 13:38:10 -0400

    quick commit

--- views/gallery.php
+++ views/gallery.php
@@ -1,39 +1,39 @@
 <?php
 
-    $sql = "SELECT * FROM youtubedatabase";
-    // $sql = "SELECT id, src, thumbnail, name FROM gallery";
+$sql = "SELECT * FROM youtubedatabase";
+// $sql = "SELECT id, src, thumbnail, name FROM gallery";
 
-    $result = $mysqli->query($sql);
+$result = $mysqli->query($sql);
+
+?>
+
+<ul class="data">
+    <?php
     
-    ?>
-
-    <ul class="data">
-        <?php
-        
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                // printf(
-                //     'id: %s, src: %s, thumbnail: %s, name: %d <br />',
-                //     $row['id'],
-                //     $row['src'],
-                //     $row['thumbnail'],
-                //     $row['name']);
-            ?>
-                <li>
-                    <p><?php echo $row['Url']?> </p>
-                </li>
-                <li>
-                    <p><?php echo $row['Url']?> </p>
-                </li>
-                <li>
-                    <p><?php echo $row['Url']?> </p>
-                </li>
-            <?php
-            }
-        } else {
-            printf('No record found.<br />');
-        }
-        mysqli_free_result($results);
-        $mysqli->close();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            // printf(
+            //     'id: %s, src: %s, thumbnail: %s, name: %d <br />',
+            //     $row['id'],
+            //     $row['src'],
+            //     $row['thumbnail'],
+            //     $row['name']);
         ?>
-    </ul>
+            <li>
+                <p><?php echo $row['Url']?> </p>
+            </li>
+            <li>
+                <p><?php echo $row['Url']?> </p>
+            </li>
+            <li>
+                <p><?php echo $row['Url']?> </p>
+            </li>
+        <?php
+        }
+    } else {
+        printf('No record found.<br />');
+    }
+    mysqli_free_result($results);
+    $mysqli->close();
+    ?>
+</ul>
--- views/showcase.php
+++ views/showcase.php
@@ -1,26 +1,39 @@
 <?php
-    $sql = "SELECT * FROM ytdatabase";
-    // SELECT * (* = all columns) FROM yourdatabase
-    $result = $mysqli->query($sql);
+
+$sql = "SELECT * FROM ytdatabase";
+// SELECT * (* = all columns) FROM yourdatabase
+$result = $mysqli->query($sql);
+
 ?>
 
 <h1>Video Entries:</h1>
 
 <?php
-    if($result->num_rows > 0){
-        while($row = $result->fetch_assoc()){
-            ?>
-            <div class="video-data">
-                <p><?php echo $row['id'];?></p>
-                <h2><?php echo $row['YoutubeVideo'];?></h2>
-                <p><?php echo $row['Category'];?></p>
-                <a href="<?php echo $row['Url'];?>"><?php echo $row['Url'];?></a>
-            </div>
-        <?php
-        }
-        } else {
-            printf('No record found.<br />');
-        }
-    mysqli_free_result($result);
-    $mysqli->close();
+
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        $embed_url = str_replace('watch?v=', 'embed/', $row['Url']);
+        ?>
+        <div class="video-data">
+            <p><?php echo $row['id'];?></p>
+            <h2><?php echo $row['YoutubeVideo'];?></h2>
+            <p><?php echo $row['Category'];?></p>
+            <a href="<?php echo $row['Url'];?>"><?php echo $row['Url'];?></a>
+
+            <iframe src="<?php echo $embed_url;?>"></iframe>
+
+            
+
+        </div>
+    <?php
+    }
+    } else {
+        printf('No record found.<br />');
+    }
+mysqli_free_result($result);
+$mysqli->close();
+
 ?>
+
+
+
